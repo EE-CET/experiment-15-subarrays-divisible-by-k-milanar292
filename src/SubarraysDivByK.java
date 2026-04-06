c997e3f4b0f176f0f0e203d28b3f1000581a8541
milanar292 <milanar292@gmail.com>
2026-04-06 23:25:22 +0530

    Update SubarraysDivByK.java

--- src/SubarraysDivByK.java
+++ src/SubarraysDivByK.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class SubarraysDivisibleByK {
+public class SubarraysDivByK {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
